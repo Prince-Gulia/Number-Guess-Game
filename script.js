bfba599185030d2f9f6e08183e21405ad559fa03
Prince Gulia <princegulia170306@gmail.com>
2025-11-07 15:09:05 +0530

    Fully Completiong the Game

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ submit.addEventListener( 'click' , ( e ) => {
     e.preventDefault()
 })
 
+function validateGuess (guess){
+    // To check if the guess is in range and valid or not
+}
+
+function checkGuess (guess){
+    //To check if guess is correct, high or low
+}
+
+function displayMessage(message){
+    //To display the message if the guess is right, wrong or correct
+}
+
+function displayDetails (guess){
+    //To display the details like previous guesses, remaining guesses and all
+}
+
+function endGame (){
+    // To end the game if user wants 
+}
+
+function newGame(){
+    // To start a new game if user wants to play again (A new button to restart again maybe if all guesses lost or if user wants)
+}
